e76ad3ffd99dd6bb5cc5cb1dacfc896d41a7c027
expectocode <expectocode@gmail.com>
2021-02-26 16:10:26 +0000

    Use DefaultOutputFormatter for digital output

--- src/main/java/uk/co/edstow/cain/scamp5/Scamp5DefaultOutputFormatter.java
+++ src/main/java/uk/co/edstow/cain/scamp5/Scamp5DefaultOutputFormatter.java
@@ -1,28 +1,24 @@
 package uk.co.edstow.cain.scamp5;
 
-import uk.co.edstow.cain.RegisterAllocator;
-
-import java.util.List;
-
 public class Scamp5DefaultOutputFormatter implements Scamp5OutputFormatter {
   @Override
-  public String nor(RegisterAllocator.Register r1, RegisterAllocator.Register r2, RegisterAllocator.Register r3) {
+  public String nor(String r1, String r2, String r3) {
     return String.format("NOR(%s, %s, %s); ", r1, r2, r3);
   }
 
   @Override
-  public String mov(RegisterAllocator.Register r1, RegisterAllocator.Register r2) {
+  public String mov(String r1, String r2) {
     return String.format("MOV(%s, %s); ", r1, r2);
   }
 
   @Override
-  public String clr(List<RegisterAllocator.Register> regs) {
+  public String clr(String... regs) {
     // Regs must be non-empty
     StringBuilder sb = new StringBuilder("CLR(");
-    for (int i = 0; i < regs.size(); i++) {
-      sb.append(String.format("%s, ", regs.get(i)));
+    for (int i = 0; i < regs.length - 1; i++) {
+      sb.append(String.format("%s, ", regs[i]));
     }
-    RegisterAllocator.Register lastReg = regs.get(regs.size() - 1);
+    String lastReg = regs[regs.length - 1];
     sb.append(lastReg);
     sb.append("); ");
 
@@ -30,12 +26,12 @@ public class Scamp5DefaultOutputFormatter implements Scamp5OutputFormatter {
   }
 
   @Override
-  public String set(RegisterAllocator.Register r1) {
+  public String set(String r1) {
     return String.format("SET(%s); ", r1);
   }
 
   @Override
-  public String dnews0(RegisterAllocator.Register r1) {
-    return String.format("DNEWS0(%s, %s); ", r1);
+  public String dnews0(String r1, String r2) {
+    return String.format("DNEWS0(%s, %s); ", r1, r2);
   }
 }
--- src/main/java/uk/co/edstow/cain/scamp5/Scamp5OutputFormatter.java
+++ src/main/java/uk/co/edstow/cain/scamp5/Scamp5OutputFormatter.java
@@ -1,13 +1,17 @@
 package uk.co.edstow.cain.scamp5;
 
-import uk.co.edstow.cain.RegisterAllocator;
+import uk.co.edstow.cain.regAlloc.RegisterAllocator;
 
 import java.util.List;
 
 public interface Scamp5OutputFormatter {
-  String nor(RegisterAllocator.Register r1, RegisterAllocator.Register r2, RegisterAllocator.Register r3);
-  String mov(RegisterAllocator.Register r1, RegisterAllocator.Register r2);
-  String clr(List<RegisterAllocator.Register> regs);
-  String set(RegisterAllocator.Register r1);
-  String dnews0(RegisterAllocator.Register r1);
+  String nor(String r1, String r2, String r3);
+
+  String mov(String r1, String r2);
+
+  String clr(String... regs);
+
+  String set(String r1);
+
+  String dnews0(String r1, String r2);
 }
